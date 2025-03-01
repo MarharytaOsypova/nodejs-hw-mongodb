1fb3af2ff800e54f20d4c54b2d6f71ce9130268d
MarharytaOsypova <osipovamv03@gmail.com>
2025-03-01 21:37:39 +0200

    mongodb-hm

---
+++ src/server.js
@@ -0,0 +1,69 @@
+
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import dotenv from "dotenv";
+import { getContactsAll, getContactsID } from './services/getcontacts.js';
+
+dotenv.config();
+
+export const setupServer =() => {
+    const app = express();
+  const PORT = process.env.PORT || 3000;
+  
+  app.get('/contacts', async (req, res) => {
+    const contacts =await getContactsAll();
+    res.status(200).json({
+      message: "Successfully found contacts!",
+      data: contacts,
+    });
+
+  });
+
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactsID(contactId);
+      
+    
+    	if (!contact) {
+	  res.status(404).json({
+		  message: 'Contact not found'
+	  });
+	  return;
+	}
+    res.status(200).json({
+  message: "Successfully found contact with id {contactId}!",
+      data: {
+    contact
+  }
+})
+})
+
+
+    app.use(cors());
+
+     app.use(
+  pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  }),
+  );
+  
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+   
+    app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+    });
+    
+     
+
+
+} 
+
+
+
