97d40de9050c22a3b3b1a1f153ea22ba7b772212
MarharytaOsypova <osipovamv03@gmail.com>
2025-03-01 22:07:13 +0200

    mongohm2

--- src/server.js
+++ src/server.js
@@ -32,7 +32,7 @@ export const setupServer =() => {
 	  return;
 	}
     res.status(200).json({
-  message: `Successfully found contact with id {contactId}!`,
+  message: `Successfully found contact with id ${contactId}!`,
       data: {
     contact
   }
