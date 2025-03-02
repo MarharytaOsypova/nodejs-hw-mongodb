cc973a5829539a63e504dc5890a93f203fe2176a
MarharytaOsypova <osipovamv03@gmail.com>
2025-03-02 17:57:35 +0200

    mongodb2

--- src/server.js
+++ src/server.js
@@ -10,10 +10,20 @@ dotenv.config();
 export const setupServer =() => {
     const app = express();
   const PORT = process.env.PORT || 3000;
+     app.use(cors());
+
+     app.use(
+  pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  }),
+  ); 
   
   app.get('/contacts', async (req, res) => {
     const contacts =await getContactsAll();
     res.status(200).json({
+      status: 200,
       message: "Successfully found contacts!",
       data: contacts,
     });
@@ -32,23 +42,11 @@ export const setupServer =() => {
 	  return;
 	}
     res.status(200).json({
+      status: 200,
   message: `Successfully found contact with id ${contactId}!`,
-      data: {
-    contact
-  }
+      data: contact
 })
-})
-
-
-    app.use(cors());
-
-     app.use(
-  pino({
-    transport: {
-      target: 'pino-pretty',
-    },
-  }),
-  );
+  })
   
   app.use('*', (req, res, next) => {
     res.status(404).json({
