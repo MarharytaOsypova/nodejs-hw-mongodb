fa740b9251fdaaf18edf7d65ed912c944758020a
MarharytaOsypova <osipovamv03@gmail.com>
2025-03-08 18:43:48 +0200

    changes3

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -18,8 +18,12 @@ export const getContactsId = async (req, res) => {
     const { contactId } = req.params;
     const contact = await getContactsID(contactId);
     	if (!contact) {
-throw createError(404, 'Student not found');
-	}
+  return res.status(404).json({
+    status: 404,
+    message: "Contact not found",
+    data: null,
+  });
+}
     res.status(200).json({
       status: 200,
   message: `Successfully found contact with id ${contactId}!`,
